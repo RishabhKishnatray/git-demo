84e889e6c489d9c432fc46d520785a02f4e07e72
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-05-25 15:08:02 +0530

    Update MavenProjectDSL.groovy

--- MavenProjectDSL.groovy
+++ MavenProjectDSL.groovy
@@ -1,15 +1,40 @@
-job('first maven') {
-  description(" First maven job bu DSL on ${new Date()} , this is a small maven project on git ")
-    Scm {
-    		git("git@github.com:opstree/spring3hibernate.git", master)
-     }
-     triggers {
-     	scm('* * * * *')
-        }
-        steps {
-        	maven(' clean package ' , 'pom.xml')
+pipeline {
+    agent any
+	tools {
+		
 	}
-    publishers {
-    	archiveArtifacts '**/*.jar'
+
+    stages {
+        stage('git checkout') {
+            steps {
+                https://github.com/opstree/spring3hibernate.git
+            }
         }
-       }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// job('first maven') {
+//   description(" First maven job bu DSL on ${new Date()} , this is a small maven project on git ")
+//     Scm {
+//     		git("git@github.com:opstree/spring3hibernate.git", master)
+//      }
+//      triggers {
+//      	scm('* * * * *')
+//         }
+//         steps {
+//         	maven(' clean package ' , 'pom.xml')
+// 	}
+//     publishers {
+//     	archiveArtifacts '**/*.jar'
+//         }
+//        }
