0ac0e9b2b4b672a8fdbc9edb928aa3ab70f420ba
RishabhKishnatray <89369743+RishabhKishnatray@users.noreply.github.com>
2023-05-25 07:33:06 +0530

    Update MavenProjectDSL.groovy

--- MavenProjectDSL.groovy
+++ MavenProjectDSL.groovy
@@ -1,7 +1,7 @@
 job('first maven') {
   description(" First maven job bu DSL on ${new Date()} , this is a small maven project on git ")
     Scm {
-    		git("https://github.com/opstree/spring3hibernate.git", master)
+    		git("git@github.com:opstree/spring3hibernate.git", master)
      }
      triggers {
      	scm('* * * * *')
